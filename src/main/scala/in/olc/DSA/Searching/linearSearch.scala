06c846c033177dd9353bbc6e1b8572b4cda44645
Prasad213 <prasad2132018@outlook.com>
2024-02-09 10:15:14 +0530

    linearSearch Updated

--- src/main/scala/in/olc/DSA/Searching/linearSearch.scala
+++ src/main/scala/in/olc/DSA/Searching/linearSearch.scala
@@ -3,11 +3,20 @@ package in.olc.DSA.Searching
 object linearSearch {
   def main(args: Array[String]): Unit = {
     val arr=Array(2,1,4,6,8,7,0,2)
-    val element=2
-    val index=search(element)
+    val element= 10
+    val index=search(arr,element)
+    println(index)
   }
-  private def sort(arr:Array[Int],element:Int):Int={
-    while()
+  private def search(arr:Array[Int],element:Int):Int={
+    var index:Int= 0
+    while((index < arr.length) && (arr(index)!=element)){
+      index+=1
+    }
+    if(index == arr.length){
+      -1
+    }else{
+      index
+    }
   }
 
 
