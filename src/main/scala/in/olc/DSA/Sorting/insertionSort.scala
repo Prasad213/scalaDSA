6c91d39f4a14b6f556c9a46864546f0c89f3b5c0
Prasad213 <prasad2132018@outlook.com>
2024-02-16 14:52:28 +0530

    insertionSort Updated

--- src/main/scala/in/olc/DSA/Sorting/insertionSort.scala
+++ src/main/scala/in/olc/DSA/Sorting/insertionSort.scala
@@ -1,5 +1,36 @@
 package in.olc.DSA.Sorting
 
-object insertionSort {
+import scala.annotation.tailrec
 
+object insertionSort {
+  def main(args: Array[String]): Unit = {
+    val arr=Array[Int](1,10,30,60,2,9,1)
+    val result=sorted(arr)
+    result.foreach(println)
+  }
+  //create part unsorted and sorted arr
+  //then take each element insert in correct position in sorted arr
+  @tailrec
+  def sorted(unsortedArr: Array[Int], sortedArr:Array[Int]=Array[Int]()):Array[Int]={
+    if(unsortedArr.isEmpty){
+      sortedArr
+    }else{
+      val element=unsortedArr.head
+      val newSortedArr=insertElement(element, sortedArr)
+      sorted(unsortedArr.tail,newSortedArr)
+    }
+  }
+
+  def insertElement(element: Int, sortedArr: Array[Int]): Array[Int] = {
+    var i = 0
+    if(sortedArr.isEmpty){
+      sortedArr :+ element
+    }else{
+      while ((i <= sortedArr.length - 1) && (sortedArr(i) < element)) {
+        i += 1
+      }
+      val parted = sortedArr.splitAt(i)
+      parted(0) :+ element :++ parted(1)
+    }
+  }
 }
