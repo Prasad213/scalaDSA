eae9f591f062a00325b79a8b86a9f42c1069326b
Prasad213 <prasad2132018@outlook.com>
2024-02-17 10:13:37 +0530

    selectionSort Updated

--- src/main/scala/in/olc/DSA/Sorting/selectionSort.scala
+++ src/main/scala/in/olc/DSA/Sorting/selectionSort.scala
@@ -1,5 +1,25 @@
 package in.olc.DSA.Sorting
 
-object selectionSort {
+import scala.annotation.tailrec
 
+object selectionSort {
+  def main(args: Array[String]): Unit = {
+    val arr = Array[Int](1, 10, 30, 60, 2, 9, 1)
+    val result = sorted(arr)
+    arr.foreach(println)
+  }
+  def sorted(arr: Array[Int]): Array[Int] = {
+    for (i <- arr.indices) {
+      var min = i
+      for (j <- (i + 1) until arr.length) {
+        if (arr(min) > arr(j)) {
+          min = j
+        }
+      }
+      val temp = arr(i)
+      arr.update(i, arr(min))
+      arr.update(min, temp)
+    }
+    arr
+  }
 }
